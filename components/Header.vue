4051206c4b18002abad6b7528295cc20373845e7
Bzheng1211 <bzheng1211@gmail.com>
2022-04-14 12:17:53 -0400

    Reputting code

---
+++ components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="head">
+        <h2 class="name">Fierfud</h2>
+        <section class="buttons">
+            <button class="btnh home">Home</button>
+            <button class="btnh blogs">Blogs</button>
+            <button class="btnh logIn">Log In</button>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Header",
+    }
+</script>
+
+<style>
+    .name{
+        color: var(--white);
+        flex: 50%;
+    }
+    .head{
+        display: flex;
+        height: 5%;
+        width: 100%;
+    }
+    .btnh{
+        font-size: var(--h2);
+
+    }
+</style>
