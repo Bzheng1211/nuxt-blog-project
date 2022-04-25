03eb726dc4ceb4d9b4dc4095bcdacc313c21580c
Bzheng1211 <bzheng1211@gmail.com>
2022-04-25 12:56:08 -0400

    Revert "Test (might need reverse)"
    This reverts commit 0c87f62670f1d0b3ae98240f3cfbb564f1d462ad.

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/content
-    "@/content",
+    "@nuxt/content",
   ],
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
