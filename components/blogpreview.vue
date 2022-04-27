dad391607e8011bbe1c0ef5a20ffc255f42f3f61
Bzheng1211 <bzheng1211@gmail.com>
2022-04-27 13:12:01 -0400

    blog preview

--- components/blogpreview.vue
+++ components/blogpreview.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-
+        <nuxt-link>
+            
+            <h3>{{ title }}</h3>
+            <p>{{description}}</p>
+        </nuxt-link>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name:"blogpreview",
+         
     }
 </script>
 
