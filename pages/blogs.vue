6de2ec78dca6e60627b2657ed61ae990e970d619
Bzheng1211 <90789894+Bzheng1211@users.noreply.github.com>
2022-06-10 11:30:18 -0400

    slug works

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -4,7 +4,7 @@
         <h2 class="name">Fierfud</h2>
         <button class="button 2"><nuxt-link to="/"><h3>Home</h3></nuxt-link></button>
         <button class="button 1" @click="changeTheme"><h3>Theme</h3></button>
-        <button class="button 3"><nuxt-link to="/blogs"><h3>Blogs</h3></nuxt-link></button>
+        <button class="button 3"><nuxt-link to="/blogs/"><h3>Blogs</h3></nuxt-link></button>
         <button class="button 3"><nuxt-link to="/admin/#/"><h3>Create Blog</h3></nuxt-link></button>
     </div>
     <form id="search">
